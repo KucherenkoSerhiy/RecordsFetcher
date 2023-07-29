85cf796ca442b38c3aa082976adfad6e4dcf4892
Serhiy Kucherenko <kucherenkoserhiy@gmail.com>
2023-07-30 01:08:29 +0200

    [feat] wrap the dependency service

---
+++ src/RecordsFetcher.Application/RemoteDataService/IRemoteDataService.cs
@@ -0,0 +1,9 @@
+﻿using RemoteServer.Models;
+
+namespace RecordsFetcher.Application.RemoteDataService
+{
+    public interface IRemoteDataService
+    {
+        DataRecord[] GetRecords(ServerDateTime notBefore, ServerDateTime notAfter, int recordLimit);
+    }
+}
---
+++ src/RecordsFetcher.Application/RemoteDataService/RemoteDataService.cs
@@ -0,0 +1,20 @@
+﻿using RemoteServer.Models;
+using RemoteServer.Services;
+
+namespace RecordsFetcher.Application.RemoteDataService
+{
+    public class RemoteDataService : IRemoteDataService
+    {
+        private readonly RemoteRecordRetriever remoteRecordRetriever;
+
+        public RemoteDataService(RemoteRecordRetriever remoteRecordRetriever)
+        {
+            this.remoteRecordRetriever = remoteRecordRetriever;
+        }
+
+        public DataRecord[] GetRecords(ServerDateTime notBefore, ServerDateTime notAfter, int recordLimit)
+        {
+            return remoteRecordRetriever.getRemoteRecords(notBefore, notAfter, recordLimit);
+        }
+    }
+}

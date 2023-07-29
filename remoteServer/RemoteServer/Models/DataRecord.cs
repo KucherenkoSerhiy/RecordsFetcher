e5ad7fddbe87c5d1a120e46ecc3e96b84dd1f946
Serhiy Kucherenko <kucherenkoserhiy@gmail.com>
2023-07-30 00:20:04 +0200

    [feat] add DataRecord model that represents the remote server's model

---
+++ remoteServer/RemoteServer/Models/DataRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace RemoteServer.Models
+{
+    public class DataRecord
+    {
+        public int ID;
+
+        public ServerDateTime CreationDate;
+
+        public byte[] Data;
+    }
+}

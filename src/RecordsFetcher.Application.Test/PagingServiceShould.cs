272f212f5ccae5ae3c1f968bb5640413da4a1f89
Serhiy Kucherenko <kucherenkoserhiy@gmail.com>
2023-07-30 01:13:13 +0200

    [feat] create empty PagingService class

---
+++ src/RecordsFetcher.Application.Test/PagingServiceShould.cs
@@ -0,0 +1,17 @@
+﻿namespace RecordsFetcher.Application.Test
+{
+    public class PagingServiceShould
+    {
+        [Fact]
+        public void GetRecords_PageOne_ReturnsCorrectRecords()
+        {
+            
+        }
+
+        [Fact]
+        public void GetRecords_PageTwo_ReturnsCorrectRecords()
+        {
+            
+        }
+    }
+}
---
+++ src/RecordsFetcher.Application/PagingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RemoteServer.Models;
+using RemoteServer.Services;
+
+namespace RecordsFetcher.Application
+{
+    public class PagingService
+    {
+        private readonly RemoteRecordRetriever remoteRecordRetriever;
+
+        public PagingService(RemoteRecordRetriever remoteRecordRetriever)
+        {
+            this.remoteRecordRetriever = remoteRecordRetriever;
+        }
+        
+        public DataRecord[] GetRecords(int pageNumber, int resultsPerPage)
+        {
+            return Array.Empty<DataRecord>();
+        }
+    }
+}

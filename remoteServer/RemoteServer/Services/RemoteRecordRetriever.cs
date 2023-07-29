8d3732e27d3d4bdfbdb96b3d797e20ddb2642ddc
Serhiy Kucherenko <kucherenkoserhiy@gmail.com>
2023-07-30 00:30:45 +0200

    [feat] add getRemoteRecords() representing a client call to the remote server

---
+++ remoteServer/RemoteServer/Services/RemoteRecordRetriever.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RemoteServer.Models;
+
+namespace RemoteServer.Services
+{
+    public class RemoteRecordRetriever
+    {
+        // Perform a remote lookup of data records from the server. 
+        // Will return at most recordLimit (or 50 records if recordLimit is not in the range [1,50]) 
+        // notBefore and notAfter are inclusive. 
+        public DataRecord[] getRemoteRecords(ServerDateTime notBefore, ServerDateTime notAfter, int recordLimit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
